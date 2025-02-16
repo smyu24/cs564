03d1ff1abbe8fb14ac2ecdebbbaaf2387f3e982a
Tony Adriansen <tony.adriansen@gmail.com>
2025-02-16 16:05:55 -0600

    schema tables in draw io file, create tables, beginning of load.txt

--- create.sql
+++ create.sql
@@ -2,32 +2,36 @@ DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Items;
 DROP TABLE IF EXISTS Categories;
 DROP TABLE IF EXISTS Bids;
-
 CREATE TABLE Users (
-    id TEXT PRIMARY KEY,
-    rating INTEGER NOT NULL,
-    location TEXT,
-    country TEXT,
+    UserID TEXT PRIMARY KEY NOT NULL,
+    Rating INTEGER NOT NULL,
+    Location TEXT,
+    Country TEXT
 );
-
 CREATE TABLE Items (
-    id TEXT PRIMARY KEY,
-    name TEXT,
-    currently DECIMAL(10,2),
-    buy_price DECIMAL(10,2),
-    first_bid DECIMAL(10,2),
-    number_of_bids INTEGER,
-    description TEXT,
-    ends REAL,
-    started REAL,
-    seller_id TEXT,
+    ItemID TEXT PRIMARY KEY,
+    Name TEXT,
+    Currently DECIMAL(10, 2),
+    Buy_Price DECIMAL(10, 2),
+    First_Bid DECIMAL(10, 2),
+    Number_of_Bids INTEGER,
+    Description TEXT,
+    Ends REAL,
+    Started REAL,
+    SellerID TEXT,
+    FOREIGN KEY SellerID REFERENCES Users(UserID)
 );
-
 CREATE TABLE Categories (
-    name TEXT PRIMARY KEY
+    ItemID TEXT,
+    Name TEXT,
+    PRIMARY KEY (ItemID, Name),
+    FOREIGN KEY (ItemID) REFERENCES Items(ItemID),
 );
-
 CREATE TABLE Bids (
-    bidder_id TEXT PRIMARY KEY,
-    
+    BidderID TEXT PRIMARY KEY NOT NULL,
+    ItemID TEXT NOT NULL,
+    Time TEXT NOT NULL,
+    Amount DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY BidderID REFERENCES Users(UserID),
+    FOREIGN KEY ItemID REFERENCES Items(ItemID)
 );
